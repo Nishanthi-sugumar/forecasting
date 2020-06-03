a45d81d922f4821508a6451d4dcae2259118ea69
Nishanthi-sugumar <58939013+Nishanthi-sugumar@users.noreply.github.com>
2020-06-03 11:25:03 +0530

    Add files via upload

---
+++ forecating1co.R
@@ -0,0 +1,143 @@
+library(readxl)
+coco<-read_xlsx(file.choose())
+
+View(coco) # Seasonality 4 quaterly
+windows()
+plot(coco$Sales,type="o")
+# So creating 4 dummy variables 
+
+Q1 <-  ifelse(grepl("Q1",coco$Quarter),'1','0')
+
+Q2 <-  ifelse(grepl("Q2",coco$Quarter),'1','0')
+Q3 <-  ifelse(grepl("Q3",coco$Quarter),'1','0')
+Q4 <-  ifelse(grepl("Q4",coco$Quarter),'1','0')
+
+
+y<-cbind(coco,Q1,Q2,Q3,Q4)
+View(y)
+colnames(y)[2]<-"sales"
+colnames(y)
+y["t"]<- 1:42
+View(y)
+y["log_rider"]<-log(y["sales"])
+y["t_square"]<-y["t"]*y["t"]
+attach(y)
+
+train<-y[1:36,]
+
+test<-y[37:42,]
+
+########################### LINEAR MODEL #############################
+
+linear_model<-lm(sales~t,data=train)
+summary(linear_model)
+linear_pred<-data.frame(predict(linear_model,interval='predict',newdata =test))
+View(linear_pred)
+rmse_linear<-sqrt(mean((test$sales-linear_pred$fit)^2,na.rm = T))
+rmse_linear # 209.9256
+
+
+
+######################### Exponential #################################
+
+expo_model<-lm(log_rider~t,data=train)
+summary(expo_model)
+expo_pred<-data.frame(predict(expo_model,interval='predict',newdata=test))
+rmse_expo<-sqrt(mean((test$sales-exp(expo_pred$fit))^2,na.rm = T))
+rmse_expo # 217.0526
+
+######################### Quadratic ####################################
+
+Quad_model<-lm(sales~t+t_square,data=train)
+summary(Quad_model)
+Quad_pred<-data.frame(predict(Quad_model,interval='predict',newdata=test))
+rmse_Quad<-sqrt(mean((test$sales-Quad_pred$fit)^2,na.rm=T))
+rmse_Quad # 137.15
+
+######################### Additive Seasonality #########################
+
+sea_add_model<-lm(sales~Q1+Q2+Q3+Q4,data=train)
+summary(sea_add_model)
+sea_add_pred<-data.frame(predict(sea_add_model,newdata=test,interval='predict'))
+rmse_sea_add<-sqrt(mean((test$sales-sea_add_pred$fit)^2,na.rm = T))
+rmse_sea_add # 264.66
+
+######################## Additive Seasonality with Linear #################
+
+Add_sea_Linear_model<-lm(sales~t+Q1+Q2+Q3+Q4,data=train)
+summary(Add_sea_Linear_model)
+Add_sea_Linear_pred<-data.frame(predict(Add_sea_Linear_model,interval='predict',newdata=test))
+rmse_Add_sea_Linear<-sqrt(mean((test$sales-Add_sea_Linear_pred$fit)^2,na.rm=T))
+rmse_Add_sea_Linear # 168.6316
+
+######################## Additive Seasonality with Quadratic #################
+
+Add_sea_Quad_model<-lm(sales~t+t_square+Q1+Q2+Q3+Q4,data=train)
+summary(Add_sea_Quad_model)
+Add_sea_Quad_pred<-data.frame(predict(Add_sea_Quad_model,interval='predict',newdata=test))
+rmse_Add_sea_Quad<-sqrt(mean((test$sales-Add_sea_Quad_pred$fit)^2,na.rm=T))
+rmse_Add_sea_Quad # 50.60725
+
+######################## Multiplicative Seasonality #########################
+
+multi_sea_model<-lm(log_rider~Q1+Q2+Q3+Q4,data = train)
+summary(multi_sea_model)
+multi_sea_pred<-data.frame(predict(multi_sea_model,newdata=test,interval='predict'))
+rmse_multi_sea<-sqrt(mean((test$sales-exp(multi_sea_pred$fit))^2,na.rm = T))
+rmse_multi_sea # 268.197
+
+######################## Multiplicative Seasonality Linear trend ##########################
+
+multi_add_sea_model<-lm(log_rider~t+Q1+Q2+Q3+Q4,data = train)
+summary(multi_add_sea_model) 
+multi_add_sea_pred<-data.frame(predict(multi_add_sea_model,newdata=test,interval='predict'))
+rmse_multi_add_sea<-sqrt(mean((test$sales-exp(multi_add_sea_pred$fit))^2,na.rm = T))
+rmse_multi_add_sea # 172.76
+
+# Preparing table on model and it's RMSE values 
+
+table_rmse<-data.frame(c("rmse_linear","rmse_expo","rmse_Quad","rmse_sea_add","rmse_Add_sea_Quad","rmse_multi_sea","rmse_multi_add_sea"),c(rmse_linear,rmse_expo,rmse_Quad,rmse_sea_add,rmse_Add_sea_Quad,rmse_multi_sea,rmse_multi_add_sea))
+View(table_rmse)
+colnames(table_rmse)<-c("model","RMSE")
+View(table_rmse)
+
+# Additive seasonality with Quadratic has least RMSE value
+
+new_model <- lm(sales~t+t_square+Q1+Q2+Q3+Q4,data=y)
+
+
+resid <- residuals(new_model)
+resid[1:10]
+windows()
+acf(resid,lag.max = 10)
+# By principal of parcimony we will consider lag - 1  as we have so 
+# many significant lags 
+# Building Autoregressive model on residuals consider lag-1 
+
+k <- arima(resid, order=c(1,0,0))
+str(k)
+
+View(data.frame(res=resid,newresid=k$residuals))
+windows()
+acf(k$residuals,lag.max = 15)
+pred_res<- predict(arima(k$residuals,order=c(1,0,0)),n.ahead = 12)
+str(pred_res)
+pred_res$pred
+acf(k$residuals)
+write.csv(y,file="y.csv",col.names = F,row.names = F)
+
+####################### Predicting new data #############################
+library(readxl)
+test_data<-read_excel(file.choose(),1)
+View(test_data)
+pred_new<-data.frame(predict(new_model,newdata=test_data,interval = 'predict'))
+View(pred_new)
+Quarter <- as.data.frame(test_data$Quarter)
+
+Final <- as.data.frame(cbind(Quarter,test_data$Sales,pred_new))
+colnames(Final) <-c("Quarter","Sales","New_Pred_Value")
+plot(Final$Sales,main = "ActualGraph", xlab="Sales(Actual)", ylab="Quarter",
+     col.axis="blue",type="o") 
+plot(Final$New_Pred_Value, main = "PredictedGraph", xlab="Sales(Predicted)", ylab="Quarter",
+     col.axis="Green",type="s")
+View(Final)
